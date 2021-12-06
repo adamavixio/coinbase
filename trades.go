5e2fb0f152cfe62569b9ded4a54dde3b9115fa99
adamavixio <adamjohnston@Adams-MacBook-Pro.local>
2021-12-05 17:05:45 -0700

    added trades to api

---
+++ trades.go
@@ -0,0 +1,30 @@
+package coinbaseclient
+
+import (
+	"encoding/json"
+	"fmt"
+
+	logger "github.com/adamavixio/logger"
+)
+
+type Trade struct {
+	Time    string `json:"time"`
+	TradeID int64  `json:"trade_id"`
+	Price   string `json:"price"`
+	Size    string `json:"size"`
+	Side    string `json:"side"`
+}
+
+func Trades(productID string, after string) []Trade {
+	params := map[string]string{}
+	params["after"] = after
+
+	path := fmt.Sprintf("/products/%s/trades", productID)
+	data := executeAuthenticatedRequest(get, path, params, nil)
+
+	trades := []Trade{}
+	err := json.Unmarshal(data, &trades)
+	logger.Error("coinbase product unmarshal error", err)
+
+	return trades
+}
